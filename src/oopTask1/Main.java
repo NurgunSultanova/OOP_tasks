45bb8956d5428cc46003621a5560a93476e5bdaa
NurgunSultanova <nurgunsultanli4@gmail.com>
2026-08-20 11:52:25 +0400

    Inital commit

--- src/oopTask1/Main.java
+++ src/oopTask1/Main.java
@@ -1,4 +1,19 @@
 package oopTask1;
 
 public class Main {
+    public static void main(String[] args) {
+        bankAccount user1=new bankAccount("Nurgun","1",1218);
+        bankAccount user2=new bankAccount("Aysel","2",2349);
+        bankAccount user3=new bankAccount("Adil","3",9823);
+
+        user1.deposit(2400.0);
+        user2.deposit(2800.0);
+        user3.deposit(3000.0);
+        user1.deposit(-400.0);
+
+        user2.transferTo(user3, 1600.0,2348);
+        user1.checkBalance(1223);
+        user3.withDraw(4500,9823);
+
+    }
 }
--- src/oopTask1/bankAccount.java
+++ src/oopTask1/bankAccount.java
@@ -1,4 +1,100 @@
 package oopTask1;
 
+import java.util.ArrayList;
+
 public class bankAccount {
+    private String accountHolder;
+    private String accountNumber;
+    private double balance;
+    private int pin;
+    private boolean isLocked;
+    private ArrayList<String> transactionHistory;
+    private double interestRate;
+    private int wrongAttemps;
+
+    public bankAccount(String accountHolder,String accountNumber,int pin){
+        this.accountHolder=accountHolder;
+        this.accountNumber=accountNumber;
+        this.pin=pin;
+        this.balance=0.0;
+        this.isLocked=false;
+        this.wrongAttemps=0;
+        this.transactionHistory=new ArrayList<>();
+        }
+
+        public void lockAccount() {
+            if (isLocked) {
+                System.out.println(" Xeberdarliq: 3 Defe yanlis sifre daxil edildi!");
+            }
+        }
+
+        public void deposit(double amount) {
+            if (amount < 0) {
+                System.out.println("Xeberdarliq: menfi mebleg gebul edilmir!");
+            } else {
+                balance += amount;
+                transactionHistory.add("Depozit: +" + amount);
+            }
+        }
+
+        public boolean withDraw(double amount,int enteredPin) {
+            if (isLocked) {
+                System.out.println("Hesab bloklanib!");
+                return false;
+            }
+            if (enteredPin == this.pin) {
+                wrongAttemps = 0;
+
+                if (amount > balance) {
+                    System.out.println("Balans kifayet etmir!");
+                    return false;
+                } else {
+                    balance -= amount;
+                    transactionHistory.add("Withdraw: -" + amount);
+                    return true;
+                }
+            } else {
+                wrongAttemps++;
+                System.out.println("Yanlis Pin!");
+                if (wrongAttemps >= 3) {
+                    lockAccount();
+                }
+                return false;
+            }
+        }
+        public void checkBalance(int enteredPin) {
+            if (enteredPin == this.pin) {
+                System.out.println("Balans: " + balance);
+            } else {
+                System.out.println("Yanlis pin!");
+            }
+        }
+        public void transferTo(bankAccount receiver,double amount,int enteredPin) {
+            if (isLocked) {
+                System.out.println("Hesab bloklanib.");
+            } else {
+                boolean success = this.withDraw(amount, enteredPin);
+                if (success) {
+                    receiver.deposit(amount);
+                    transactionHistory.add("Transfer to " + receiver.accountHolder + ": -" + amount);
+                    System.out.println("Kocurme ugurlu oldu.");
+                } else {
+                    System.out.println("Kocurme ugursuz oldu.");
+                }
+            }
+        }
+        public void applyInterest() {
+        if (isLocked){
+            System.out.println("Hesab bloklanib!");
+        } else {
+            balance +=balance*interestRate/100;
+            System.out.println("Faiz hesablandiqdan sonra balans: "+balance);
+        }
+    }
+    public void printHistory() {
+        System.out.println(" Emeliyyat tarixcesi");
+        for (String transaction : transactionHistory) {
+            System.out.println(transaction);
+        }
+    }
 }
